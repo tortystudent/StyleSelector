2d6276e37d0df7df2d41ae4930132718702258d3
Simon Anthony <github@torty.org.uk>
2017-11-25 21:57:59 +1100

    end saturday 11  25

--- index.php
+++ index.php
@@ -19,95 +19,153 @@ include "index_header.php";
         </div>
         <!-- Left Off Page -->
         <?php if ($this->countModules('left-off-page')) : ?>
-          <div class='left-off-page'>
-            <jdoc:include type="modules" name="left-off-page" /> </div>
-          <?php endif; ?>
-               <?
+        <div class='left-off-page'>
+          <jdoc:include type="modules" name="left-off-page" /> </div>
+        <?php endif; ?>
+        <?
     include "index_sizes.php"
     ?>
-            <div class="grid">
-              <header_left>
+          <div class="grid">
+
+            <section >
+              <div class="header_left">
                 <name>
-                  <h1>header-left</h1> </name>
-                <jdoc:include type="modules" name="header-left" class="header-left" style="xhtml" /> </header_left>
-              <header_right>
+                  <h1>header-left</h1>
+                </name>
+                <jdoc:include type="modules" name="header-left" class="header-left" style="xhtml" />
+              </div>
+            </section>
+             <section>
+              <div class="header_right">
                 <name>
-                  <h1>header-right</h1> </name>
+                  <h1>header-right</h1>
+                </name>
                 <a href='<?php echo JURI::base(); ?>'>
                   <jdoc:include type="modules" name="header-right" class="header-right" style="xhtml" /> </a>
-              </header_right>
-              <nav>
+              </div>
+            </section>
+
+            <nav>
+              <name>
+                <h1>nav</h1>
+              </name>this is the nav area
+              <jdoc:include type="modules" name="navigation" style="xhtml" />
+            </nav>
+
+            <section>
+             <div class="hero_full_width">
+              <name>
+                <h1>hero-full-width</h1>
+              </name>
+              <jdoc:include type="modules" name="hero-full-width" style="xhtml" />
+              </div>
+            </section>
+
+            <section class="above_content">
+              <name>
+                <h1>above-content</h1>
+              </name>
+              <jdoc:include type="modules" name="above-content" style="xhtml" />
+            </section>
+
+            <section class="main">
+              <main class="article">
                 <name>
-                  <h1>nav</h1> </name>
-                <jdoc:include type="modules" name="navigation" style="xhtml" /> </nav>
-              <section>
-                <hero_full_width>
-                  <name>
-                    <h1>hero-full-width</h1> </name>
-                  <jdoc:include type="modules" name="hero-full-width" style="xhtml" /> </hero_full_width>
-              </section>
-              <section>
-                <above_content>
-                  <name>
-                    <h1>above-content</h1> </name>
-                  <jdoc:include type="modules" name="above-content" style="xhtml" /> </above_content>
-              </section>
-              <section>
-                <main class="article">
-                  <name>
-                    <h1>main content</h1> </name>
-                  <!-- Begin Content -->
-                  <jdoc:include type="message" />
-                  <jdoc:include type="component" />
-                  <!-- End Content -->
-                </main>
-              </section>
-              <section>
-                <below_content>
-                  <name>
-                    <h1>below-content</h1> </name>
-                  <jdoc:include type="modules" name="below-content" style="xhtml" /> </below_content>
-              </section>
-              <section>
-                <below_content-1>
-                  <name>
-                    <h1>below-content-1</h1> </name>
-                  <jdoc:include type="modules" name="below-content-1" style="xhtml" /> </below_content-1>
-              </section>
-              <section>
-                <below_content-2>
-                  <name>
-                    <h1>below-content-2</h1> </name>
-                  <jdoc:include type="modules" name="below-content-2" style="xhtml" /> </below_content-2>
-              </section>
-              <section>
-                <below_content-3>
-                  <name>
-                    <h1>below-content-3</h1> </name>
-                  <jdoc:include type="modules" name="below-content-3" style="xhtml" /> </below_content-3>
-              </section>
-              <section>
-                <footer>
-                  <name>
-                    <h1>footer</h1> </name>
-                  <jdoc:include type="modules" name="footer" style="xhtml" />hand added content to footer </footer>
-                <footer_left>
-                  <name>
-                    <h1>footer-left</h1> </name>
-                  <jdoc:include type="modules" name="footer-left" style="xhtml" /> </footer_left>
-                <footer_right>
-                  <name>
-                    <h1>footer-right</h1> </name>
-                  <jdoc:include type="modules" name="footer-right" style="xhtml" /> </footer_right>
-              </section>
-              <section>
-                <float_bottom>
-                  <name>
-                    <h1>flt_btm</h1> </name>
-                  <jdoc:include type="modules" name="float-bottom" style="xhtml" />
-                  <h3>hand added content to float_bottom </h3> </float_bottom>
-              </section>
-            </div>
+                  <h1>main content</h1>
+                </name>
+                <!-- Begin Content -->
+                <jdoc:include type="message" />
+                <jdoc:include type="component" />
+                <!-- End Content -->
+              </main>
+            </section>
+
+            <section class="below_content">
+              <name>
+                <h1>below-content</h1>
+              </name>
+              <jdoc:include type="modules" name="below-content" style="xhtml" />
+            </section>
+
+            <section class="below_content-1">
+              <name>
+                <h1>below-content-1</h1>
+              </name>
+              <jdoc:include type="modules" name="below-content-1" style="xhtml" />
+              <div class="card">
+                <div class="card-body">
+                  This is some text within a card block.
+                </div>
+              </div>
+              <div class="card">
+                <div class="card-body">
+                  This is some text within a card block.
+                </div>
+              </div>
+              <div class="card">
+                <div class="card-body">
+                  This is some text within a card block.
+                </div>
+              </div>
+              <div class="card">
+                <div class="card-body">
+                  This is some text within a card block.
+                </div>
+              </div>
+            </section>
+
+            <section class="below_content-2">
+              <name>
+                <h1>below-content-2</h1>
+              </name>
+              <jdoc:include type="modules" name="below-content-2" style="xhtml" />
+              <div class="card" style="width: 20rem;">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                  <h4 class="card-title">Sample Card Title</h4>
+                  <p class="card-text">He seems sinking under the evidence could not only grieve and a visit. The father is to bless and placed in his length hid...</p>
+                  <a href="#" class="btn btn-primary">Tell me more &rarr;</a>
+                </div>
+              </div>
+            </section>
+
+            <section class="below_content-3">
+              <name>
+                <h1>below-content-3</h1>
+              </name>
+              <jdoc:include type="modules" name="below-content-3" style="xhtml" />
+            </section>
+
+            <section class="footer">
+              <name>
+                <h1>footer</h1>
+              </name>
+              <jdoc:include type="modules" name="footer" style="xhtml" />hand added content to footer
+
+              <div class="footer_left">
+                <name>
+                  <h1>footer-left</h1>
+                </name>
+                <jdoc:include type="modules" name="footer-left" style="xhtml" />
+              </div>
+
+              <div class="footer_right">
+                <name>
+                  <h1>footer-right</h1>
+                </name>
+                <jdoc:include type="modules" name="footer-right" style="xhtml" />
+              </div>
+            </section>
+
+            <section class="float_bottom">
+              <name>
+                <h1>flt_btm</h1>
+              </name>
+              <jdoc:include type="modules" name="float-bottom" style="xhtml" />
+              <h3>hand added content to float_bottom </h3>
+            </section>
+
+          </div>
       </div>
     </div>
   </body>
